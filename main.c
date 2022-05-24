01dc97dad1976bc2a7f80463cb35ae2358920c74
Jarno Rohmert <jarno.rohmert@wms-os.de>
2022-05-24 10:59:51 +0200

    Added Gitignore

--- main.c
+++ main.c
@@ -9,53 +9,88 @@
 
 /**
  *
- *
+ *test
  **/
-int day_of_the_year(int day, int month, int year)
+
+
+void input_date(int *zeiger_Tag, int *zeiger_Monat,  int *zeiger_Jahr)
 {
-    int tage_pro_monat[12] = {31,28,31,30,31,30,31,31,30,31,30,31}; //Menge der Tage in einem Monat
-    int tag = 0; //Eingabe des Tages im Monat
-    int monat = 0;  //Eingabe des Monats im Jahr
-    int jahr = 0;   //Eingabe des gewünschten Jahres
-    int ausgabe = 0;    //ausgabe des gerechneten TAges
+    //*zeiger_Tag;
+    //*zeiger_Monat;
+    //*zeiger_Jahr;
+    printf("Geben Sie einen Tag ein\n");
+    scanf("%i",zeiger_Tag);
+    printf("Geben Sie einen Monat ein\n");
+    scanf("%i",zeiger_Monat);
+    printf("Geben Sie ein Jahr ein\n");
+    scanf("%i",zeiger_Jahr);
+}
 
-    printf("Geben Sie das Jahr ein \n");
-    scanf("%i", &jahr); //Eingabe des gewünschten Jahres
-    if(jahr%4 == 0)     //Falls das Jahr durch 4 Teilbar ist
+int is_leapyear(int year)
+{
+    int leap;
+    if(year%4 == 0)     //Falls das Jahr durch 4 Teilbar ist
     {
-        //tage_pro_monat[1] = 28;    //Kein Schaltjahr
+        leap = 0;
 
-        if (jahr%100 ==0)  //Falls das Jahr durch 100 Teilbar ist
+        if (year%100 ==0)  //Falls das Jahr durch 100 Teilbar ist
         {
-            //tage_pro_monat[1] = 29;    //Schlatjahr
+            leap = 1;
 
-            if(jahr%400 == 0)    //Falls das JAhr durch 400 Teilbar ist
+            if(year%400 == 0)    //Falls das JAhr durch 400 Teilbar ist
             {
-                tage_pro_monat[1] = 29;    //Schaltjahr
+                leap = 1;
             }
         }
+    }else{
+        leap=0;
     }
-    printf("Geben Sie bitte den Monat ein (als Zahl) \n");
-    scanf("%i", &monat);    //Eingabe des Monats
-    monat = monat-1;    //Für das Array wird -1 gerechnet
-    printf("Geben Sie bitte den Tag ein \n");
-    scanf("%i", &tag);  //Eingabe des Tages
-    while(tag>tage_pro_monat[monat])    //Der TAg muss im Array liegen
+    return leap;
+}
+int get_days_for_month(int month,int year, int *month_array)
+{
+    int eingabe_monat;
+    eingabe_monat = month-1;
+    printf(eingabe_monat);
+    printf(month_array);
+    if(is_leapyear(year)==1){
+        month_array[1] = 29;
+    }
+    printf("\n");
+    printf(month_array);
+    return month_array[eingabe_monat];
+}
+int day_of_the_year(int day,int month,int year)
+{
+    int tage_pro_monat[12] = {31,28,31,30,31,30,31,31,30,31,30,31}; //Menge der Tage in einem Monat
+    int ausgabe = 0;    //ausgabe des gerechneten TAges
+    int *pTage_pro_monat = &month;
+    int monats_tage = get_days_for_month(month,year,tage_pro_monat);
+
+    while(day>monats_tage)    //Der TAg muss im Array liegen
     {
-        printf("Tag zu groß für Monat \n");
-        scanf("%i", &tag);  //Erneute eingabe bis der Tag im Monat liegt
+        printf("Tag zu groï¿½ fï¿½r Monat \n");
+        scanf("%i", &day);  //Erneute eingabe bis der Tag im Monat liegt
     }
-    for(int i = 0; i<monat; i++)    //Hoch addieren der Zahlen bis alle Monate drin sind
+    for(int i = 0; i<(month-1); i++)    //Hoch addieren der Zahlen bis alle Monate drin sind
     {
-        ausgabe = ausgabe + tage_pro_monat[i];
+        ausgabe = ausgabe + pTage_pro_monat[i];
     }
-    ausgabe = ausgabe + tag;    //Tag wird addiert
+    ausgabe = ausgabe + monats_tage;    //Tag wird addiert
     printf("Das ist der %i . Tag des Jahres", ausgabe); //Ausgabe der gesamten TAge
-    return 0;
+    return ausgabe;
 }
 
 int main()
 {
-    day_of_the_year(1,1,1);
+    int day ,*pday = &day;
+    int month ,*pmonth = &month;
+    int year ,*pyear = &year;
+    int tage_pro_monat[12] = {31,28,31,30,31,30,31,31,30,31,30,31}; //Menge der Tage in einem Monat
+    input_date(pday,pmonth,pyear);
+    is_leapyear(pyear);
+    printf(is_leapyear(pyear));
+    get_days_for_month(pmonth,pyear,&tage_pro_monat);
+    day_of_the_year(pday,pmonth,pyear);
     return 0;
 }
